e3c8c9e332615698874b6da1338a321a7564a8c7
Joe Chandler <joseph-chandler@sky.com>
2026-05-16 13:01:54 +0100

    Updates for issues with mac os transparency

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -15,6 +15,52 @@ return {
   },
 
   {
+    "xiyaowong/transparent.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("transparent").setup {
+        extra_groups = {
+          -- main UI
+          "NormalFloat",
+          "FloatBorder",
+          "CursorLine",
+          "StatusLine",
+          "StatusLineNC",
+          "SignColumn",
+          "EndOfBuffer",
+          -- NvChad / tree
+          "NvimTreeNormal",
+          "NvimTreeNormalNC",
+          "NvimTreeWinSeparator",
+          "NvimTreeEndOfBuffer",
+        },
+      }
+
+      -- force transparency AFTER colorscheme loads
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        callback = function()
+          local groups = {
+            "Normal",
+            "NormalNC",
+            "NormalFloat",
+            "FloatBorder",
+            "SignColumn",
+            "EndOfBuffer",
+            "NvimTreeNormal",
+            "NvimTreeNormalNC",
+            "NvimTreeEndOfBuffer",
+          }
+
+          for _, group in ipairs(groups) do
+            vim.api.nvim_set_hl(0, group, { bg = "none" })
+          end
+        end,
+      })
+    end,
+  },
+
+  {
     "windwp/nvim-ts-autotag",
     ft = { "html", "xml", "javascriptreact", "typescriptreact" },
     config = function()
