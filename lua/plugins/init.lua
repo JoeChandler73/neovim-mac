81e80c73dcb6f8b048838c7ab575688901397659
Joe Chandler <joseph-chandler@sky.com>
2026-05-16 21:21:28 +0100

    Added keybinding to open floating toggle term

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -32,6 +32,14 @@ return {
   },
 
   {
+    "voldikss/vim-floaterm",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>o", "<cmd>FloatermToggle<cr>", desc = "Terminal" },
+    },
+  },
+
+  {
     "stevearc/conform.nvim",
     event = "BufWritePre",
     opts = require "configs.conform",
